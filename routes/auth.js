4fae6b137b087648bfff630d1ba35f1cd04ed0c5
mac <mac@macs-MacBook-Pro.local>
2023-06-05 11:48:39 -0700

    Final All Commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  logout,
+} = require("../controllers/auth");
+const { getMe } = require("../controllers/auth");
+
+const router = express.Router();
+const { protect } = require("../middleware/auth");
+router.post("/register", register);
+router.post("/login", login);
+router.post("/login", logout);
+router.get("/me", protect, getMe);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
+router.put("updatepassword", protect, updatePassword);
+module.exports = router;
